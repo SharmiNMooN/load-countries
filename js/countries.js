f8a16b2596438681f168f7b729be129c6b32034e
Sharmin Moon <sharminmoon0308@gmail.com>
2022-09-19 18:59:13 +0600

    flag added

--- js/countries.js
+++ js/countries.js
@@ -4,6 +4,17 @@ const loadCountries = () => {
     .then((data) => displayCountries(data));
 };
 const displayCountries = (countries) => {
-  console.log(countries);
+  //   console.log(countries);
+  const countriesHTML = countries.map((country) => getCountryHTML(country));
+  console.log(countriesHTML[0]);
+  const container = document.getElementById("countries");
+  container.innerHTML = countriesHTML.join(" ");
+};
+const getCountryHTML = (country) => {
+  return `
+    <div class = "country">
+    <h2>${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    </div>`;
 };
 loadCountries();
